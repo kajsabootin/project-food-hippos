25370f8f272f309c6ad58cc05da2385913aee476
Christina Persson <christina@Christinas-MacBook.local>
2020-02-25 12:48:01 +0100

    Ready for merge

--- script.js
+++ script.js
@@ -12,14 +12,14 @@ fetch(url, { headers: { "user-key": apiKey } })
     json.restaurants.forEach(resto => {
       console.log(resto.restaurant.name);
       restaurantContainer.innerHTML += `<a href= ${resto.restaurant.url} class="resturant-card">
-        <img class="restaurant-image" src= ${resto.restaurant.photos[0].photo.thumb_url}> 
+        <img class="restaurant-image" src= ${resto.restaurant.photos[0].photo.thumb_url}>
+        <h3>${resto.restaurant.name}</h3> 
 
-
-      <h3>${resto.restaurant.name}</h3> 
-
-
+      <div class="b-a-container">
       <p class="address">${resto.restaurant.location.address}</p>
-      <button class="booking-button">KNAPPEN</button>
+      <button class="booking-button">Book</button>
+      </div>
+
       <br>
       <p class="average-cost">${resto.restaurant.average_cost_for_two} ${resto.restaurant.user_rating.aggregate_rating}</p>
       
